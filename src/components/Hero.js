af3f46719ea8814043e11df0287e66eea898777a
Jesus Salomon <salomonj1121@gmail.com>
2022-10-19 23:17:24 -0500

    added home page hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import heroBcg from '../assets/hero-bcg.jpeg'
-import heroBcg2 from '../assets/hero-bcg-2.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import heroBcg from '../assets/hero-bcg.jpeg';
+import heroBcg2 from '../assets/hero-bcg-2.jpeg';
 
 const Hero = () => {
-  return <h4>hero</h4>
-}
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          Design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quidem placeat maxime temporibus vitae magnam eos optio
+          libero culpa iure voluptas!
+        </p>
+        <Link to="/products" className="btn heron-btn">
+          Shop now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="Nice table" className="main-img" />
+        <img
+          src={heroBcg2}
+          alt="person working"
+          className="accent-img"
+        />
+      </article>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   min-height: 60vh;
@@ -68,6 +93,6 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
   }
-`
+`;
 
-export default Hero
+export default Hero;
