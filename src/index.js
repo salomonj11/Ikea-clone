7dc290c4708d2acb3353e51a561aa448af1ef016
Jesus Salomon <salomonj1121@gmail.com>
2022-10-31 15:42:03 -0500

    set up auth0

--- src/index.js
+++ src/index.js
@@ -9,12 +9,19 @@ import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-b46dekyeauuc8cng.us.auth0.com"
+    clientId="9z8m7VGkObk0Mmj5tEtsNwANbjD381Uz"
+    redirectUri={window.location.origin}
+    catcheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
