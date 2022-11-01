950b6ee292eed54bbf0c555b0658c3bf33789c0e
Jesus Salomon <salomonj1121@gmail.com>
2022-11-01 18:15:09 -0500

    refactored to react router 6, ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
 } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
-import styled from 'styled-components';
-
 import {
   Home,
   SingleProduct,
@@ -16,40 +14,33 @@ import {
   About,
   Products,
   PrivateRoute,
+  AuthWrapper,
 } from './pages';
-
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/products">
-          <Products />
-        </Route>
-        <Route
-          exact
-          path="/products/:id"
-          children={<SingleProduct />}
-        />
-        <PrivateRoute exact path="/checkout">
-          <Checkout />
-        </PrivateRoute>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
+          <Route
+            path="checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="error" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
